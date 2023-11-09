5116c6c82a74f27e224e0d50626e6f32b4015c10
gkdey17cse <gkdey17cse@gmail.com>
2023-11-09 12:08:04 +0530

    updation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["./index.html","./todolist.html","./landingpage.html"],
   theme: {
     extend: {},
   },
